66c6445f89bfb248e2553b81f1c980a5f1728abf
pandabot-nu <81609018+pandabot-nu@users.noreply.github.com>
2021-07-05 03:09:22 +1000

    Delete database directory

--- database/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-module.exports =
-    mongoose.connect(process.env.MONGODB_SRV, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-    }
-    )
