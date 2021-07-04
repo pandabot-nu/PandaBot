5270914ba9f7ee3b5a334988c29e1ccab9ec4fb6
pandabot-nu <81609018+pandabot-nu@users.noreply.github.com>
2021-07-05 03:06:10 +1000

    Update guildMemberAdd.js
    removed profileData

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -11,17 +11,10 @@ module.exports = async (Discord, client, member) => {
         .setTitle('Welcome to Nook University!')
         .setDescription(`Hi there ${member}! \n` +
             `Welcome to ${member.guild.name} - a great 25+ server where we relax and have fun! \n` +
-            `Can you please tell us your age, and a modmin will be around shortly to welcome you and provide access to the rest of the server! <:a:isawave:783091772217098311>`)
+            `Can you please tell us your age, and a modmin will be around shortly to welcome you and provide access to the rest of the server! <a:isawave:783091772217098311>`)
         .setThumbnail('https://media.discordapp.net/attachments/798082988230967307/811266489108660274/ACbanner2.jpg')
 
     channel.send(welcomeEmbed);
 
-    let profile = await profileModel.create({
-        userID: member.id,
-        userName: member.name,
-        serverID: member.guild.id,
-        coins: 1000,
-        bank: 0,
-    });
-    profile.save();
+   
 }
