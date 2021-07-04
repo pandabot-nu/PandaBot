d52aafee0170d7485e5d3e0c7ef65d09215bbe39
pandabot-nu <81609018+pandabot-nu@users.noreply.github.com>
2021-07-05 03:05:29 +1000

    Update ready.js
    removed database logging

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,5 +2,5 @@ const database = require('../../database/database')
 
 module.exports = () => {
     console.log('PandaBot is online!')
-    database.then(() => console.log("Connected to DB.")).catch(err => console.log(err));
+    
 }
