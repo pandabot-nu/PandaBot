c53975d61a1286acac8745a78ca78230b98f2167
pandabot-nu <81609018+pandabot-nu@users.noreply.github.com>
2021-07-05 17:12:48 +1000

    Create serverinfo.js
    serverinfo command added

---
+++ commands/utility/serverinfo.js
@@ -0,0 +1,36 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+    name: "serverinfo",
+    aliases: ["si", "serveri", "sinfo"],
+    permissions: ["ADMINISTRATOR"],
+    description: 'Gives Info About The Server', // Optional
+ execute(client, message, args, Discord) {
+
+        const botSize = message.guild.members.cache.filter(m => m.user.bot).size
+
+const { guild } = message
+const emojicount = message.guild.emojis.cache 
+const members = message.guild.members.cache
+        const { name, owner, roles, channels, createdAt, premiumSubscriptionCount, memberCount, region, server } = message.guild
+        message.channel.send(
+            new Discord.MessageEmbed()
+                .setTitle(name)
+                .setDescription(`Some simple server information.`)
+                .setThumbnail(message.guild.iconURL({ dynamic: true }))
+                .addField('Owner', owner.displayName, true)
+                .addField('Boosters', premiumSubscriptionCount, true)
+                .addField('Boost Level:-', message.guild.premiumTier, true)
+                .addField('Region', region.toUpperCase(), true)
+                .addField('Roles', roles.cache.size, true)
+                .addField('Server ID', message.guild.id, true)
+                .addField('Member Count:-', `Total: ${members.size} | Human: ${members.filter(member => !member.user.bot).size} | Bot: ${members.filter(member => member.user.bot).size}`) 
+                .addField('Emoji Count:-', `Total: ${emojicount.size} | Non Animated: ${emojicount.filter(emoji => !emoji.animated).size} | Animated: ${emojicount.filter(emoji => emoji.animated).size}`)
+                .addField('Channel Count:-', `Category: ${guild.channels.cache.filter(channel => channel.type == 'category').size} | Text: ${guild.channels.cache.filter(channel => channel.type == 'text').size} | Voice: ${guild.channels.cache.filter(channel => channel.type == 'voice').size}`)
+                .setFooter(`Created on`)
+                .setColor('RANDOM')
+                .setTimestamp(createdAt)
+        )
+            .catch((e) => message.channel.send(`error: ${e.message}`))
+    }
+}
