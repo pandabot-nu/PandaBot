f29f13d9d165b24f0cea40693fb9b6cd21aa78bc
pandabot-nu <81609018+pandabot-nu@users.noreply.github.com>
2021-07-05 15:59:49 +1000

    Update work.js
    updated jobs to working with NPCs

--- commands/economy/work.js
+++ commands/economy/work.js
@@ -8,11 +8,11 @@ module.exports = {
     cooldown: 60000 * 15,
     description: "Adds coins to a members balance.",
     async execute(client, message, args, Discord, profileData) {
-      const jobs = ["Doctor", "Nurse"]
+      const jobs = ["Wisp", "Celeste", "Blathers", "Tom Nook", "Isabelle", "C.J", "Flick", "Mable", "Sable", "Label", "Timmy","Tommy", "K.K Slider", "Sahara", "Daisy Mae", "Leif", "Redd"]
       const jobIndex = Math.floor(Math.random() * jobs.length);
       const coins = Math.floor(Math.random() * (500-200)) + 200;
       
-      message.channel.send(`You worked as a ${jobs[jobIndex]} and earned ${coins} coins`);
+      message.channel.send(`You worked with ${jobs[jobIndex]} and earned ${coins} coins`);
       const guildId = message.guild.id
       const userId = message.author.id
       
