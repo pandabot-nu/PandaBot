fa6f881bee97e5de6e6c8565ee97396ba5060d47
pandabot-nu <81609018+pandabot-nu@users.noreply.github.com>
2021-07-07 00:01:11 +1000

    Create drop.js
    needs testing

---
+++ commands/economy/drop.js
@@ -0,0 +1,29 @@
+const profileModel = require('../../database/models/profileSchema')
+const economy = require('../../database/economy')
+const { execute } = require('./slots')
+module.exports = {
+    name: "drop",
+    aliases: [],
+    usage: "[channel] [drop amount]", 
+    permissions: ["ADMINISTRATOR"],
+    description: "Drops PandaCoins in a specified channel",
+
+    async execute(client, message, args, Discord, profileData) {
+        const channel = message.mentions.channels.first();
+        if(!channel) return message.reply("Please mention a channel to drop the PandaCoins in!")
+
+        const coins = args[1]
+        if(!coins||isNaN(coins)) return message.reply("Please specify a valid amount of PandaCoins!")
+
+        const filter = (msg) => msg.guild.id === message.guild.id && msg.content === `${prefix}pick`
+
+        channel.awaitMessages(filter, {max: 1, time: 60000}).then(async (msg) =>{
+            const userId = msg.first().author.id
+            const guildId = message.guild.id
+            const claimCoins = parseInt(coins)
+
+            economy.addCoins(guildId, userId, claimCoins)
+            msg.first().reply(`${msg.first().author} has picked ${claimCoins} PandaCoins!`)
+        })
+    }
+}
