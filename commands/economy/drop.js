4a48e8fc422f6aa8914ee26965b3d763823fef19
pandabot-nu <81609018+pandabot-nu@users.noreply.github.com>
2021-07-07 00:17:12 +1000

    Update drop.js
    updated and working

--- commands/economy/drop.js
+++ commands/economy/drop.js
@@ -15,9 +15,9 @@ module.exports = {
         const coins = args[1]
         if (!coins || isNaN(coins)) return message.reply("Please specify a valid amount of PandaCoins!")
 
-        const filter = (msg) => msg.guild.id === message.guild.id && msg.content === `${prefix}pick`
-        message.channel.send(`${coins} PandaCoins have been dropped in ` + channel.toString)
-        channel.send(`Quick! ${coins} PandaCoins have been dropped!! Type ${prefix}pick to pick them up!!`)
+        const filter = (msg) => msg.guild.id === message.guild.id && msg.content === `${process.env.PREFIX}pick`
+        message.channel.send(`${coins} PandaCoins have been dropped in ${channel}`)
+        channel.send(`Quick! ${coins} PandaCoins have been dropped!! Type ${process.env.PREFIX}pick to pick them up!!`)
 
         channel.awaitMessages(filter, { max: 1, time: 60000 }).then(async (msg) => {
             const userId = msg.first().author.id
