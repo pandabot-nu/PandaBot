84203018ae2b880fb321b965e15b10a1d4b1e7f7
pandabot-nu <81609018+pandabot-nu@users.noreply.github.com>
2021-07-07 00:12:14 +1000

    Update drop.js
    updated - pending test

--- commands/economy/drop.js
+++ commands/economy/drop.js
@@ -4,20 +4,22 @@ const { execute } = require('./slots')
 module.exports = {
     name: "drop",
     aliases: [],
-    usage: "[channel] [drop amount]", 
+    usage: "[channel] [drop amount]",
     permissions: ["ADMINISTRATOR"],
     description: "Drops PandaCoins in a specified channel",
 
     async execute(client, message, args, Discord, profileData) {
         const channel = message.mentions.channels.first();
-        if(!channel) return message.reply("Please mention a channel to drop the PandaCoins in!")
+        if (!channel) return message.reply("Please mention a channel to drop the PandaCoins in!")
 
         const coins = args[1]
-        if(!coins||isNaN(coins)) return message.reply("Please specify a valid amount of PandaCoins!")
+        if (!coins || isNaN(coins)) return message.reply("Please specify a valid amount of PandaCoins!")
 
         const filter = (msg) => msg.guild.id === message.guild.id && msg.content === `${prefix}pick`
+        message.channel.send(`${coins} PandaCoins have been dropped in ` + channel.toString)
+        channel.send(`Quick! ${coins} PandaCoins have been dropped!! Type ${prefix}pick to pick them up!!`)
 
-        channel.awaitMessages(filter, {max: 1, time: 60000}).then(async (msg) =>{
+        channel.awaitMessages(filter, { max: 1, time: 60000 }).then(async (msg) => {
             const userId = msg.first().author.id
             const guildId = message.guild.id
             const claimCoins = parseInt(coins)
