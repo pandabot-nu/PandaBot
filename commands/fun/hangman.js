3b7a4f287e3e48da0644b33c7c89840fc53d2ab3
pandabot-nu <81609018+pandabot-nu@users.noreply.github.com>
2021-07-09 17:23:57 +1000

    Update hangman.js
    added embeds

--- commands/fun/hangman.js
+++ commands/fun/hangman.js
@@ -1,71 +1,94 @@
 const words = require('../../utils/words.json');
 const { stripIndents } = require('common-tags')
 module.exports = {
-    name: "hangman",
-    aliases: ["hm"],
-    usage: [],
-    permissions: [],
-    description: "Plays a game of hangman with Animal Crossing words",
+  name: "hangman",
+  aliases: ["hm"],
+  usage: [],
+  permissions: [],
+  description: "Plays a game of hangman with Animal Crossing words",
 
-    async execute(client, message, args, Discord) {
-        const word = words[Math.floor(Math.random() * words.length)].toLowerCase();
-        let points = 0;
-        let displayText = null;
-        let guessed = false;
-        const confirmation = [];
-        const incorrect = [];
-        const display = new Array(word.length).fill('_');
-        while (word.length !== confirmation.length && points < 7) {
-            await message.channel.send(stripIndents`
-                ${displayText === null ? '**Here we go!**' : displayText ? '**Good job!**' : '**Nope!**'}
-                \`${display.join(' ')}\`. **Which letter do you choose**?
-                **Incorrect Tries:** ${incorrect.join(', ') || '**None**'}
-                \`\`\`
-                ___________
+  async execute(client, message, args, Discord) {
+    const word = words[Math.floor(Math.random() * words.length)].toLowerCase();
+    let points = 0;
+    let displayText = null;
+    let guessed = false;
+    const confirmation = [];
+    const incorrect = [];
+    const display = new Array(word.length).fill('**_** ');
+    const user = message.author
+    const userId = user.id
+    const space = "```"
+    while (word.length !== confirmation.length && points < 7) {
+
+      const hangmanEmbed = new Discord.MessageEmbed()
+        .setTitle(`${displayText === null ? '**Here we go!**' : displayText ? '**Good job!**' : '**Nope!**'}`)
+        .setColor("RANDOM").setDescription(`${display.join(' ')} **Which letter do you choose**?\n
+                **Incorrect Tries:** ${incorrect.join(', ') || '**None**'}`)
+        .addField(`\u200B`, stripIndents`${space}
+                 ___________
                 |     ${points > 0 ? '|' : ''}
                 |     ${points > 1 ? 'O' : ''}
                 |    ${points > 3 ? '—' : ' '}${points > 2 ? '|' : ''}${points > 4 ? '—' : ''}
                 |    ${points > 5 ? '/' : ''} ${points > 6 ? '\\' : ''}
-                ===========
-                \`\`\`
-            `);
-            const filter = res => {
-                const choice = res.content.toLowerCase();
-                return !confirmation.includes(choice) && !incorrect.includes(choice);
-            };
-            const guess = await message.channel.awaitMessages(filter, {
-                max: 1,
-                time: 30000
-            });
-            if (!guess.size) {
-                await message.channel.send('**Sorry, time is up!**');
-                break;
-            }
-            const choice = guess.first().content.toLowerCase();
-            if (choice === 'end') break;
-            if (choice.length > 1 && choice === word) {
-                guessed = true;
-                break;
-            } else if (word.includes(choice)) {
-                displayText = true;
-                for (let i = 0; i < word.length; i++) {
-                    if (word.charAt(i) !== choice) continue; // eslint-disable-line max-depth
-                    confirmation.push(word.charAt(i));
-                    display[i] = word.charAt(i);
-                }
-            } else {
-                displayText = false;
-                if (choice.length === 1) incorrect.push(choice);
-                points++;
-            }
+                ===========${space}`);
+      await message.channel.send(hangmanEmbed)
+      const filter = res => {
+        const choice = res.content.toLowerCase();
+        return !confirmation.includes(choice) && !incorrect.includes(choice);
+      };
+      const guess = await message.channel.awaitMessages(filter, {
+        max: 1,
+        time: 300000
+      });
+      if (!guess.size) {
+        await message.channel.send('**Sorry, time is up!**');
+        break;
+      }
+      const choice = guess.first().content.toLowerCase();
+      if (choice === 'end') break;
+      if (choice.length > 1 && choice === word) {
+        guessed = true;
+        break;
+      } else if (word.includes(choice)) {
+        displayText = true;
+        for (let i = 0; i < word.length; i++) {
+          if (word.charAt(i) !== choice) continue; // eslint-disable-line max-depth
+          confirmation.push(word.charAt(i));
+          display[i] = word.charAt(i);
         }
-
-        if (word.length === confirmation.length || guessed) {
-            return message.channel.send(stripIndents`
-                **You won, it was ${word}!**`);
-        }
-        return message.channel.send(stripIndents`
-            **Too bad... It was ${word}**...<a:bearlaughing:827654744642289716>`);
-
+      } else {
+        displayText = false;
+        if (choice.length === 1) incorrect.push(choice);
+        points++;
+      }
     }
+
+    if (word.length === confirmation.length || guessed) {
+      let url = client.users.fetch(userId);
+      url.then(function(targetURL) {
+        var imgURL = targetURL.displayAvatarURL();
+
+        let winEmbed = new Discord.MessageEmbed()
+          .setAuthor(`${user.username}#${user.discriminator}`, `${imgURL}`)
+          .setDescription(`**You won, it was ${word}!**`)
+          .setThumbnail("https://i.imgur.com/dUoJGTf.png")
+          .setColor("RANDOM")
+        message.channel.send(winEmbed);
+        return
+      })
+    } else {
+
+      let url = client.users.fetch(userId);
+      url.then(function(targetURL) {
+        var imgURL = targetURL.displayAvatarURL();
+
+        let loseEmbed = new Discord.MessageEmbed()
+          .setAuthor(`${user.username}#${user.discriminator}`, `${imgURL}`)
+          .setDescription(`**Too bad... It was ${word}**...<a:bearlaughing:827654744642289716>`)
+          .setThumbnail("https://i.imgur.com/q8exlJR.gif")
+          .setColor("RANDOM")
+        message.channel.send(loseEmbed); return
+      })
+    }
+  }
 }
