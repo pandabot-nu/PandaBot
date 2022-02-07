db4243e5bf811f35e98a80cae5cd7bd6b0987d20
pandabot-nu <81609018+pandabot-nu@users.noreply.github.com>
2022-02-07 14:45:43 +1100

    Create isabelle.js

---
+++ utils/isabelle.js
@@ -0,0 +1,16 @@
+[
+"No breaking news today! Is it just me, or did this week fly by? I know how hard all of you worked, so I hope you enjoy a well-deserved lovely weekend!",
+"The latest copy of my favourite magazine arrived today. I'm looking forward to reading it later!",
+"Oh! Did anyone else see that talk show yesterday? On TV? You know the one. I like watching for the guests, but the hosts are such chatterboxes!",
+"Listen to this! I was having a hard time with my crossword puzzle, but I finally figured it out!",
+"Oh yeah! Did anyone see that movie that was on TV yesterday? It's that one they play all the time. Even though I've seen it a million times, I never see that final twist coming. I'm still reeling!",
+"Oh! Did anyone else catch that quiz show on TV yesterday? How do they come up with all those questions each week? Let alone all the answers...",
+"Here's some personal news! I got to do a video chat with my family back home! Mum and Dad are both great!",
+"By the way, did anyone tune in for that documentary show on TV yesterday? It was all about the history of volleyball. And actually, it was quite informative. For one thing, I learned that talking about volleyball puts me right to sleep!",
+"Have you ever peeled some fruit and managed to keep the whole peel connected? Earlier I just did that!",
+"My favourite band was one of the featured acts and their music's still stuck in my head.",
+"Hey! Did anyone else watch that variety show on TV yesterday? The stand-up comedian they had on was fantastic! I laughed for an hour straight!",
+"I looked up my horoscope for the day and it said I'm going to have good luck!",
+"A sock I've been looking for forever turned up behind my washing machine. It's one of my favourites!",
+"But it is the start of a new week, and I hope it lives up to each and every one of your expectations!"
+]
