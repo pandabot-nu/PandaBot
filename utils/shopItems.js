3cd20ced06a0bde2ac0480198cd9f64f88c54aa7
pandabot-nu <81609018+pandabot-nu@users.noreply.github.com>
2022-02-07 14:46:11 +1100

    Create shopItems.js

---
+++ utils/shopItems.js
@@ -0,0 +1,80 @@
+module.exports = [
+    {
+        number: "1",
+        item: "5 furniture/clothing items",
+        price: "5000",
+        description: "Hunting for a specific item? We've got you covered!\n• available stock: unlimited"
+    },
+    {
+        number: "2",
+        item: "10 furniture/clothing items",
+        price: "9500",
+        description: "Hunting for a specific item? We've got you covered!\n• available stock: unlimited"
+    },
+    {
+        number: "3",
+        item: "20 furniture/clothing items",
+        price: "19000",
+        description: "Hunting for a specific item? We've got you covered!\n• available stock: unlimited"
+    },
+    {
+        number: "4",
+        item: "100 NMT",
+        price: "30000",
+        description: "100 Nook Mile Tickets for your dreamie hunt!\n• available stock: unlimited"
+    },
+    {
+        number: "5",
+        item: "Half an inventory of materials",
+        price: "40000",
+        description: "Need some materials? Let us know which ones!\n• available stock: unlimited"
+    },
+    {
+        number: "6",
+        item: "DIY set (non-seasonal)",
+        price: "50000",
+        description: "Any normal DIY set of your choice, we got you!\n• available stock: unlimited"
+    },
+    {
+        number: "7",
+        item: "Inventory of materials",
+        price: "70000",
+        description: "Need some materials? Let us know which ones!\n• available stock: unlimited"
+    },
+    {
+        number: "8",
+        item: "DIY set (seasonal)",
+        price: "80000",
+        description: "Any seasonal DIY set of your choice, we got you!\n• available stock: unlimited"
+    },
+    {
+        number: "9",
+        item: "400 NMT",
+        price: "90000",
+        description: "400 Nook Mile Tickets for your dreamie hunt!\n• available stock: unlimited"
+    },
+    {
+        number: "10",
+        item: "4 million Bells",
+        price: "120000",
+        description: "Time to become a bell-ionaire!\n• available stock: unlimited"
+    },
+    {
+        number: "11",
+        item: "DIY set + Materials",
+        price: "130000",
+        description: "Any DIY set of your choice, we got you! Need the mats too? Look no further, we can deliver those too!\n• available stock: unlimited"
+    },
+    {
+        number: "12",
+        item: "Villager in boxes",
+        price: "200000",
+        description: "Tell us your Dreamie and we will get them in boxes for you!\n• available stock: unlimited"
+    },
+    {
+        number: "13",
+        item: "40 Royal Crowns (12 million bells value)",
+        price: "300000",
+        description: "You're royalty now, love! Deck out your villagers or get those bells.\n• available stock: unlimited"
+    },
+    ]
