5aceaec20b6738d74d4190b91f1790771da58da6
pandabot-nu <81609018+pandabot-nu@users.noreply.github.com>
2022-02-07 14:15:52 +1100

    Create acWordleGuess.js

---
+++ commands/fun/acWordleGuess.js
@@ -0,0 +1,22 @@
+const answers = require('../../utils/acWordleWords.json')
+//const wordleCD = require('../../database/models/wordleCD.js')
+//const stats = require('../../database/models/wordleStats.js')
+const Discord = require('discord.js')
+const canvas = require('canvas')
+const Wordle = require('../../utils/acWordle.js')
+
+module.exports = {
+    name: "guess",
+    aliases: [],
+    usage: [],
+    permissions: [],
+    cooldown: 0,
+    description: "wordle using animal crossing villagers and NPC's!",
+
+    async execute(client, message, args, Discord) {
+
+        if (message.content.includes("+guess")) {
+            Wordle.PlayWordle(message);
+        }
+    }
+}
