67b8cf0139a033866943c0cdf699fc388c7af708
pandabot-nu <81609018+pandabot-nu@users.noreply.github.com>
2022-03-22 16:24:22 +1100

    Update coinflip.js

--- commands/economy/coinflip.js
+++ commands/economy/coinflip.js
@@ -5,13 +5,22 @@ module.exports = {
     name: "coinflip",
     aliases: ["cf"],
     permissions: [],
-    description: "flip a coin and gamble PandaCoins",
+    cooldown: 0,
+    usage: "[heads/tails] [bet amount]",
+    description: "flips a coin to gamble PandaCoins",
     async execute(client, message, args, Discord, profileData) {
+      if (message.channel.id !== '868444993340182558')
+        return;
         const guildId = message.guild.id
         const userId = message.author.id
+        const user = message.author
+
+        let thumb = ["https://i.imgur.com/Kkn7A8G.png", "https://i.imgur.com/OQo1xkQ.png"]
+        let pic = thumb[Math.floor(Math.random() * thumb.length)]
+
         if (args[0] != undefined) {
             let choice = args[0].toLowerCase();
-           
+
             if (choice == 'heads' || choice == 'h' || choice == 'head') {
                 choice = 'heads';
             }
@@ -25,23 +34,84 @@ module.exports = {
                 "heads",
                 "tails"
             ];
-            if (!args[1]) return message.reply("Please specify an amount to bet!")
-            if (isNaN(args[1])) return message.reply("Your bet must be a number amount!")
+            if (!args[1] || isNaN(args[1])) {
+                let url = client.users.fetch(userId);
+                url.then(function (targetURL) {
+                    var imgURL = targetURL.displayAvatarURL();
+                    let fEmbed = new Discord.MessageEmbed()
+                        .setAuthor(`${user.username}#${user.discriminator}`, `${imgURL}`)
+                        .setColor("RED")
+                        .setDescription('Please provide a vaild amount of PandaCoins to bet!')
+                        .setThumbnail(`${pic}`)
+                    message.channel.send(fEmbed)
+                })
+                return
+            }
 
             const coins = parseInt(args[1]);
-            if (await economy.getCoins(message.author.id) < coins) return message.reply(`You do not have ${coins} PandaCoins to bet. Please check your balance and try again.`)
-            flipResult = flipChoice[Math.floor(Math.random() * flipChoice.length)];
+            if (await economy.getCoins(guildId, userId) < coins) {
+                let url = client.users.fetch(userId);
+                url.then(function (targetURL) {
+                    var imgURL = targetURL.displayAvatarURL();
+                    let cEmbed = new Discord.MessageEmbed()
+                        .setAuthor(`${user.username}#${user.discriminator}`, `${imgURL}`)
+                        .setColor("RED")
+                        .setDescription(`You do not have ${coins} PandaCoins to bet. Please check your balance and try again.`)
+                        .setThumbnail(`${pic}`)
+                    message.channel.send(cEmbed)
+                })
+                return
+            }
+            else {
 
-            if (choice != flipResult) {
-                message.channel.send(`The flip was ${flipResult} and you chose ${choice}. You have lost ${coins} PandaCoins....Better luck next time....`);
-                economy.rmvCoins(guildId, userId, coins);
-            } else {
-                message.channel.send(`The flip was ${flipResult} and you chose ${choice}. You have won ${coins} PandaCoins! Congratulations!!`);
-                economy.addCoins(guildId, userId, coins);
-            };
+                flipResult = flipChoice[Math.floor(Math.random() * flipChoice.length)];
+
+                if (choice != flipResult) {
+
+                    let url = client.users.fetch(userId);
+                    url.then(function (targetURL) {
+                        var imgURL = targetURL.displayAvatarURL();
+
+                        let loseEmbed = new Discord.MessageEmbed()
+                            .setAuthor(`${user.username}#${user.discriminator}`, `${imgURL}`)
+                            .setDescription(`The flip was ${flipResult} and you chose ${choice}. You have lost **${coins} PandaCoins**....Better luck next time....`)
+                            .setThumbnail("https://i.imgur.com/q8exlJR.gif")
+                            .setColor("RANDOM")
+                        message.channel.send(loseEmbed)
+                        economy.rmvCoins(guildId, userId, coins);
+                    })
+                }
+
+                else {
+                    let url = client.users.fetch(userId);
+                    url.then(function (targetURL) {
+                        var imgURL = targetURL.displayAvatarURL();
+
+                        let winEmbed = new Discord.MessageEmbed()
+                            .setAuthor(`${user.username}#${user.discriminator}`, `${imgURL}`)
+                            .setDescription(`The flip was ${flipResult} and you chose ${choice}. You have won **${coins} PandaCoins!** Congratulations!!`)
+                            .setThumbnail("https://i.imgur.com/dUoJGTf.png")
+                            .setColor("RANDOM")
+                        message.channel.send(winEmbed)
+                        economy.addCoins(guildId, userId, coins);
+                    })
+
+                };
+            }
         }
+
         else {
-            message.reply("please choose **heads** or **tails** before the coin flip.");
+            let url = client.users.fetch(userId);
+            url.then(function (targetURL) {
+                var imgURL = targetURL.displayAvatarURL();
+                let qEmbed = new Discord.MessageEmbed()
+                    .setAuthor(`${user.username}#${user.discriminator}`, `${imgURL}`)
+                    .setColor("RED")
+                    .setDescription('Please choose **heads** or **tails** before the coin flip.')
+                    .setThumbnail(`${pic}`)
+                message.channel.send(qEmbed)
+                return
+            })
         }
     }
 }
