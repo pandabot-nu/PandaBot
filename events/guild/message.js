b83b87f8fd10002000eadc8c9692c98bde18c9c9
pandabot-nu <81609018+pandabot-nu@users.noreply.github.com>
2022-03-22 16:35:55 +1100

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,46 +1,89 @@
 require('dotenv').config();
-const cooldowns = new Map();
+const cooldown = require('../../database/models/cooldown')
+const Levels = require('discord-xp');
+
 
 module.exports = async (Discord, client, message) => {
+    const user = await Levels.fetch(message.author.id, message.guild.id)
+    if(message.author.bot) return
+    if (!user) { Levels.createUser(message.author.id, message.guild.id) }
+    if (Date.now() - user.lastUpdated > 60000) {
+        const randomAmountOfXp = Math.floor(Math.random() * 29) + 1; // Min 1, Max 30
+        const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
+        if (hasLeveledUp) {
+
+
+            //  message.channel.send(`${message.author}, congratulations! You have leveled up to **${user.level}**. :tada:`);
+        }
+    }
+
     const prefix = (process.env.PREFIX)
 
     if (!message.content.startsWith(prefix) || message.author.bot) return
 
-      const args = message.content.slice(prefix.length).split(/\s+/)
+    const args = message.content.slice(prefix.length).split(/\s+/)
     const cmd = args.shift().toLowerCase()
 
-    const command = client.commands.get(cmd)|| client.commands.find(a => a.aliases && a.aliases.includes(cmd))
+    const command = client.commands.get(cmd) || client.commands.find(a => a.aliases && a.aliases.includes(cmd))
 
- //If cooldowns map doesn't have a command.name key then create one.
-    if(!cooldowns.has(command.name)){
-        cooldowns.set(command.name, new Discord.Collection());
+    async function commandExecute() {
+        if (!message.member.permissions.has(command.permissions || [])) return message.channel.send(`PandaBot says you do not have permission to use this command!`)
+        if (command) command.execute(client, message, args, Discord, cmd)
+    }
+    if (command.cooldown) {
+        const current_time = Date.now();
+        const cooldown_amount = (command.cooldown)
+
+        cooldown.findOne({ userId: message.author.id, cmd: command.name }, async (err, data) => {
+            let thumb = ["https://i.imgur.com/Kkn7A8G.png", "https://i.imgur.com/OQo1xkQ.png"]
+            let pic = thumb[Math.floor(Math.random() * thumb.length)]
+            if (data) {
+                const expiration_time = data.time + cooldown_amount;
+
+                if (current_time < expiration_time) {
+                    const time_left = (expiration_time - current_time) / 1000
+
+                    if (time_left.toFixed(1) >= 3600) {
+                        let hour = (time_left.toFixed(1) / 3600);
+                        let hEmbed = new Discord.MessageEmbed()
+                        .setColor("RED")
+                            .setDescription(`**Please wait ${parseInt(hour)} more hours before using** ***${command.name}!***`)
+                            .setThumbnail(`${pic}`)
+                        message.channel.send(hEmbed); return
+                    }
+                    if (time_left.toFixed(1) >= 60) {
+                        let minute = (time_left.toFixed(1) / 60);
+                        let mEmbed = new Discord.MessageEmbed()
+                        .setColor("RED")
+                            .setDescription(`**Please wait ${parseInt(minute)} more minutes before using** ***${command.name}!***`)
+                            .setThumbnail(`${pic}`)
+                        message.channel.send(mEmbed); return
+
+                    }
+                    let seconds = (time_left.toFixed(1));
+                    let sEmbed = new Discord.MessageEmbed()
+                       .setColor("RED")
+                        .setDescription(`**Please wait ${parseInt(seconds)} more seconds before using** ***${command.name}!***`)
+                        .setThumbnail(`${pic}`)
+                    message.channel.send(sEmbed); return
+
+                } else {
+                    await cooldown.findOneAndUpdate({ userId: message.author.id, cmd: command.name }, { time: current_time });
+                    commandExecute();
+                }
+            } else {
+                commandExecute();
+                new cooldown({
+                    userId: message.author.id,
+                    cmd: command.name,
+                    time: current_time,
+                    cooldown: command.cooldown,
+                }).save();
+            }
+        })
+    } else {
+        commandExecute();
     }
 
-    const current_time = Date.now();
-    const time_stamps = cooldowns.get(command.name);
-    const cooldown_amount = (command.cooldown);
+}
 
-    //If time_stamps has a key with the author's id then check the expiration time to send a message to a user.
-    if(time_stamps.has(message.author.id)){
-        const expiration_time = time_stamps.get(message.author.id) + cooldown_amount;
-
-        if(current_time < expiration_time){
-            const time_left = (expiration_time - current_time) / 60000;
-
-            return message.reply(`Please wait ${time_left.toFixed(1)} more minutes before using ${command.name}`);
-        }
-    }
-
-    //If the author's id is not in time_stamps then add them with the current time.
-    time_stamps.set(message.author.id, current_time);
-    //Delete the user's id once the cooldown is over.
-    setTimeout(() => time_stamps.delete(message.author.id), cooldown_amount);
-
-    try{     
-        command.execute(client, message, args, Discord, cmd)
-    } catch (err){
-        message.reply("There was an error trying to execute this command!");
-        console.log(err);
-    }
-    }
-    
