967bc1c419f9dd04cbf973850b199e27dfa1f97a
pandabot-nu <81609018+pandabot-nu@users.noreply.github.com>
2022-03-22 16:22:24 +1100

    Update beg.js

--- commands/economy/beg.js
+++ commands/economy/beg.js
@@ -1,18 +1,29 @@
 const profileModel = require(`../../database/models/profileSchema`)
 const economy = require('../../database/economy')
 module.exports = {
-    name: "beg",
-    aliases: [],
-    permissions: [],
-    description: "beg for coins",
-    async execute(client, message, args, Discord, profileData) {
-        const randomNumber = Math.floor(Math.random() * 500) + 1;
+  name: "beg",
+  aliases: [],
+  cooldown: 60000 * 15,
+  permissions: [],
+  description: "Begs PandaBot for PandaCoins",
+  async execute(client, message, args, Discord, profileData) {
+   
+    const randomNumber = Math.floor(Math.random() * 500) + 1;
+    const user = message.author
+    const guildId = message.guild.id
+    const userId = message.author.id
+    const newCoins = await economy.addCoins(guildId, userId, randomNumber)
 
-        const guildId = message.guild.id
-        const userId = message.author.id
-        const newCoins = await economy.addCoins(guildId, userId, randomNumber)
+    let url = client.users.fetch(userId);
+    url.then(function (targetURL) {
+      var imgURL = targetURL.displayAvatarURL();
 
-        return message.channel.send(`${message.author.username}, you begged and received ${randomNumber} **coins**`);
-    },
-};
-
+      let begEmbed = new Discord.MessageEmbed()
+        .setAuthor(`${user.username}#${user.discriminator}`, `${imgURL}`)
+        .setDescription(`${user.username}, you begged and received **${randomNumber} PandaCoins**`)
+        .setThumbnail("https://i.imgur.com/DAHZPF4.png")
+        .setColor("RANDOM")
+      return message.channel.send(begEmbed)
+    })
+  },
+}
